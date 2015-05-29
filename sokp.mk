332498eca581b15a67ed3066531a948605633445
fizbanrapper <jeremiah.akers@outlook.com>
2015-05-29 09:23:46 -0400

    update configuration

--- sokp.mk
+++ sokp.mk
@@ -3,7 +3,12 @@ $(call inherit-product, device/htc/m8/full_m8.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/sokp/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common SOKP stuff.
 $(call inherit-product, vendor/sokp/config/common_full_phone.mk)
 
 PRODUCT_NAME := sokp_m8
+PRODUCT_DEVICE := m8
+PRODUCT_BRAND := htc
+PRODUCT_MANUFACTURER := htc
+PRODUCT_MODEL := m8
+
